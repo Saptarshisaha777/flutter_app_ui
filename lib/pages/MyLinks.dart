3f3cd2566fcb82bf018513e77f8091fcec40e237
Saptarshi <34470655+Saptarshisaha777@users.noreply.github.com>
2020-09-05 22:11:42 +0530

    Update MyLinks.dart

--- lib/pages/MyLinks.dart
+++ lib/pages/MyLinks.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/Widgets/User_view.dart';
+import 'package:flutter_app/Widgets/Underlings_view.dart';
 import 'package:flutter_app/bloc_navigation_bloc/navigation_bloc.dart';
 import 'package:flutter_app/models/Ids.dart';
 import 'package:flutter_app/models/model.dart';
@@ -21,7 +22,7 @@ class _MyLinksState extends State<MyLinks> {
   Widget build(BuildContext context) {
     print(widget.user.id);
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.lightBlue[50],
       appBar: AppBar(
         elevation: 0,
         leading: SizedBox(width: 20,),
@@ -30,9 +31,14 @@ class _MyLinksState extends State<MyLinks> {
             icon: Icon(Icons.arrow_back, color: Colors.blueGrey,),
             iconSize: 30,
             onPressed: () {
-              Navigator.pop(
-                context,
-              );
+              if (widget.user == currentUser){
+             }
+              else{
+                Navigator.pop(
+                  context,
+                );
+              }
+
             },
           )
         ],
@@ -51,72 +57,11 @@ class _MyLinksState extends State<MyLinks> {
       body: Column(
         children: <Widget>[
           Userview(main_user: widget.user,),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).accentColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-
-                ),
-              ),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical : 25.0),
-                      child: Container(
-                        height: 150,
-                        color: Theme.of(context).accentColor,
-                        child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: widget.user.links?.length ?? 1,
-                            itemBuilder: (BuildContext context,int index){
-                              String nam = net[widget?.user?.links[index] ?? 0].name ?? 'noone';
-                              return GestureDetector(
-                                onTap:  () {
-                                  print(net[widget.user.links[index]].name);
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => MyLinks(user: net[widget.user.links[index]],
-                                        )),
-                                  );
-                                  },
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric( horizontal : 10.0),
-                                  child: Column(
-                                    children: <Widget>[
-                                        CircleAvatar(
-                                          radius: 35,
-                                          child: Icon(
-                                            Icons.perm_identity,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      SizedBox(
-                                        height: 5,
-                                      ),
-                                      Text(
-                                        //index.toString(),
-                                        nam,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
-
-                            }),
-                      ),
-                    ),
-                  ],
-                ), // The Custom links
-            ),
-
-          ),
+          Underlings(widget: widget),
 
         ],
       )
     );
   }
 }
+
