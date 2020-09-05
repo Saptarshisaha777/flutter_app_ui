33199e8b863ccb86b4a72284aadb831438805698
Saptarshi <34470655+Saptarshisaha777@users.noreply.github.com>
2020-09-05 22:03:44 +0530

    Update navigation_bloc.dart

--- lib/bloc_navigation_bloc/navigation_bloc.dart
+++ lib/bloc_navigation_bloc/navigation_bloc.dart
@@ -2,10 +2,12 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_app/models/Ids.dart';
 import 'package:flutter_app/models/model.dart';
 import 'package:flutter_app/pages/Homepage.dart';
+import 'package:flutter_app/pages/Payments.dart';
 import '../pages/MyLinks.dart';
 enum NavigationEvents {
   HomePageClickedEvent,
   MyLinkClickedEvent,
+  PaymentsEvent,
 }
 
 
@@ -30,6 +32,8 @@ class NavigationBloc extends Bloc<NavigationEvents,NavigationStates> {
       case NavigationEvents.HomePageClickedEvent: yield Homepage();
         break;
       case NavigationEvents.MyLinkClickedEvent : yield MyLinks(user: currentUser);
+      break;
+      case NavigationEvents.PaymentsEvent : yield Payments();
       break;
     }
     throw UnimplementedError();
