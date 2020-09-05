98f1d0e10a5a78d7200cf1a06e7659b6e9f2e8f1
Saptarshi <34470655+Saptarshisaha777@users.noreply.github.com>
2020-09-05 22:10:16 +0530

    Update User_view.dart

--- lib/Widgets/User_view.dart
+++ lib/Widgets/User_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/Ids.dart';
 import 'package:flutter_app/models/model.dart';
 
 class Userview extends StatelessWidget {
@@ -9,12 +10,57 @@ class Userview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var test = main_user;
+    List<int> tree = [];
+    print(test.parent);
+    while(test.parent != null && tree.length < 5){
+      print('now parent is ${test.parent}');
+      tree.add(test.parent);
+      test = net[test.parent];
+    }
+    //print('the tree is');
+    print(tree);
     //var us = main_user?.name ?? 'No name';
     return Container(
-      height: 200,
+      height: 300,
       color: Colors.white70,
       child: Column(
         children: <Widget>[
+          Container(
+            height: 110,
+            color: Colors.white70,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              child: ListView.builder(
+                shrinkWrap: true,
+                  reverse: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: tree.length,//prime.length,
+                  itemBuilder: (BuildContext context,int index){
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 7),
+                      child: Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 35,
+                            child: Icon(
+                              Icons.perm_identity,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            //prime[index].name?? 'default',
+                            net[tree[index]].name,
+                          ),
+                        ],
+                      ),
+                    );
+              }),
+            ),
+          ),
           Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
@@ -23,7 +69,7 @@ class Userview extends StatelessWidget {
                   Icons.perm_identity,
                   color: Colors.white,
                 ),
-                radius: 70,
+                radius: 60,
               ),
             ),
           ),
